724153264254b6975b39e937e82bb85f5af3fa22
Amaan Jamil <80788403+Cimyrocks@users.noreply.github.com>
2023-11-20 16:13:38 -0500

    potential buffer overflow

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,29 @@
+#include <cstdio>
+#include <cstring>
 #include <iostream>
 
-class MyBadClass {
-  private:
-    int val;
-  public:
-    void setval(int val) { this->val = val;}
-}
+const char *PASSWORD_FILE = "rictro";
 
-int main() {
-    return 0;
+int main()
+{
+  char input[8];
+  char password[8];
+
+  std::sscanf(PASSWORD_FILE, "%s", password);
+
+  std::cout << "Enter password: ";
+  std::cin >> input;
+
+  // Debug prints:
+  // std::cout << "Address of input: " << &input << "\n";
+  // std::cout << "Address of password: " << &password << "\n";
+  // std::cout << "Input: " << input << "\n";
+  // std::cout << "Password: " << password << "\n";
+
+  if (std::strncmp(password, input, 8) == 0)
+    std::cout << "Access granted\n";
+  else
+    std::cout << "Access denied\n";
+
+  return 0;
 }
